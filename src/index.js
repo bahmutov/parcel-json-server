6baf4bfa1bfc7f982b745a98d32224a24d68fbd9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-08-16 11:56:25 -0400

    feat: add json-server-reset

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 const Bundler = require('parcel-bundler')
 const jsonServer = require('json-server')
 const debug = require('debug')('@bahmutov/parcel-json-server')
+const reset = require('json-server-reset')
+const setApp = require('json-server-reset/src/set-app')
+const bodyParser = require('json-server/lib/server/body-parser')
 
 const start = (application, db, port) => {
   debug('starting parcel-json-server on port %d', port)
   debug('bundling %s', application)
   debug('serving REST API based on %s', db)
 
-  const options = {}
-  const bundler = new Bundler(application, options)
-
   const server = jsonServer.create()
   const router = jsonServer.router(db)
-  const middlewares = jsonServer.defaults()
+  server.use(setApp(server, router.db))
+  server.use(bodyParser)
+  server.use(reset)
 
-  // TODO add json-server-reset middleware
-
+  const options = {}
+  const bundler = new Bundler(application, options)
   const bundlerMiddleware = bundler.middleware()
 
   server.use((req, res, next) => {
+    // TODO determine the path from loaded database resources
     if (req.path === '/todos') {
       // data request, continue to JSON server
       return next()
@@ -28,7 +31,7 @@ const start = (application, db, port) => {
     return bundlerMiddleware(req, res, next)
   })
 
-  server.use(middlewares)
+  server.use(jsonServer.defaults())
   server.use(router)
   server.listen(port, () => {
     console.log('parcel-json-server is running at port %d', port)
